9d44df6bb0b8b182d531780780520df79f3fcdc9
Aistė Stačiokaitė <Aiste.Staciokaite@bta.lt>
2022-02-25 20:49:27 +0200

    Introduction, Data Types and Variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,77 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// What is Java?
+        //Java is an object-oriented programming language that allows us to model
+        //real life functionality as objects in our code.
+
+        //Features of Java
+        //Java is a strictly-typed language
+        //Java is also a compiled language, it is turned into bytecode
+        //Java is a high-level language.
+        //Java is run on the JVM (Java Virtual Machine).
+        //Java JVM creates platform-independence for Java.
+
+        //What is programming?
+        //Computer programming is a process of giving a computer or IT device a series of instructions
+        //that tell it the order and process in which to carry out or execute a specific task or set of
+        // types.
+
+        //Concepts of programming in Java.
+        //variables and data types
+        //conditionals
+        //loops
+        //methods
+        //classes
+
+        //Variables and Datatypes
+        // primitive values
+        // char
+        // int (Integer)
+        //short
+        //byte
+        //long
+        //float // has a max of 7
+        //double
+
+        //Complex Objects
+        //Strings
+
+        //Variable: Think of a variable like an empty box, where as soon as you create the box,
+        //you decide what type of items you can put in the box and you cannot put a different type
+        // of item later on.
+
+        // P.s. You cannot have a variable with the same name in the same scope in Java.
+
+        // A variable declaration is simply giving your variable a name and a data type.
+        //A variable initialization is the point where you finally give that variable a value to start with.
+
+
+        //int number; //variable declaration
+        //number = 4; //variable initialization
+
+        //int value = 6; // variable declaration and initialization in one line.
+        //byte byteValue = 1;
+
+        //char charValue = '9';
+        //String text = "I am going to school";
+
+       //long digits = 5L;
+        //float decimalFloat = 3.683f;
+        //double decimalDouble = 5.2323235353;
+
+          // Shortcut to comment a line of code: CTRL + /
+
+        System.out.println("Hello World!");
+    }
+
+    // Arithmetic Operators include:
+    //+- Addition
+    //-- Subtraction
+    // / Division
+    // *-Multiplication
+    // % - Modulus (It divides the values and returns the remainder) e.g 2 % 2-> 0
+}
+s
